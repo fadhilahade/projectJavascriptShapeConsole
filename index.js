543d2c5b6a5f81d6bb107c4224f02c17a2dd2cce
fadhilahade <dhila.alkautsar@gmail.com>
2019-06-25 13:58:05 +0700

    first comment

---
+++ index.js
@@ -0,0 +1,13 @@
+const rectangleWidth = 50
+const rectangleLength = 20
+
+const rectangleArea = rectangleWidth * rectangleLength
+
+console.log(`${rectangleArea} = ${rectangleWidth} * ${rectangleLength}`)
+
+const triangleWidth = 82
+const triangleLength = 15
+
+const triangleArea = (1 / 2) * triangleWidth * triangleLength
+
+console.log(`${triangleArea} = 1/2 *  ${triangleWidth} * ${triangleLength}`)
